66f6793cb3c828a2357773bcb9ac699425b3ebcb
Hack Reactor Students <github@hackreactor.com>
2013-08-12 16:20:00 -0700

    Implemented a color dancer that will change colors over time

---
+++ src/colorDancer.js
@@ -0,0 +1,23 @@
+var ColorDancer = function(top, left, timeBetweenSteps){
+  this.colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'orange', 'purple', 'pink', 'turquoise'];
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.colorChanger();
+};
+
+ColorDancer.prototype = Object.create(Dancer.prototype);
+ColorDancer.prototype.constructor = ColorDancer;
+
+ColorDancer.prototype.step = function(timeBetweenSteps){
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this, timeBetweenSteps);
+};
+
+ColorDancer.prototype.colorChanger = function(){
+  var dancer = this;
+  setInterval(function() {
+    var styleSettings = {
+      'border-color': dancer.colors[Math.floor(Math.random() * dancer.colors.length)]
+    };
+    dancer.$node.css(styleSettings);
+  }, 2000);
+};
