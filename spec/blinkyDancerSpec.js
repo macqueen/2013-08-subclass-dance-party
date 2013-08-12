fff1bf9c0253ab2f21f6a5fb5f3e888ea64fff51
Hack Reactor Students <github@hackreactor.com>
2013-08-12 15:24:26 -0700

    added missing semicolon

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -22,7 +22,7 @@ describe("blinkyDancer", function() {
   describe("dance", function(){
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
-      expect(blinkyDancer.step.callCount).to.be.equal(0)
+      expect(blinkyDancer.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
