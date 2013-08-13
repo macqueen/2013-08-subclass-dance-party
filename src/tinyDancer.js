8506b9db16cf3f02417a25a51d06aac8e10f57c2
Hack Reactor Students <github@hackreactor.com>
2013-08-13 11:02:48 -0700

    added tiny dancer audio

--- src/tinyDancer.js
+++ src/tinyDancer.js
@@ -1,7 +1,7 @@
 var eltonJohnUrl = 'http://assets.rollingstone.com/assets/images/story/weekend-rock-question-what-is-elton-johns-best-song-20121123/1000x306/20121121-elton-x600-1353526455.jpg';
 
 var TinyDancer = function(top, left, timeBetweenSteps){
-  Dancer.call(this, top, left, timeBetweenSteps, $('<img class="tinyDancer" src="' + eltonJohnUrl + '"></img>'));
+  Dancer.call(this, top, left, timeBetweenSteps, $('<img class="tinyDancer" src="' + eltonJohnUrl + '"><audio src="tinyDancer.mp3" autoplay></audio></img>'));
 };
 
 TinyDancer.prototype = Object.create(Dancer.prototype);
