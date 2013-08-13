00e3153395d926135229c1ad86b3602c08d97fe4
Hack Reactor Students <github@hackreactor.com>
2013-08-12 17:02:48 -0700

    made squareDancer square dance

--- src/squareDancer.js
+++ src/squareDancer.js
@@ -1,5 +1,6 @@
 var SquareDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps, $('<span class="squareDancer"></span>'));
+  this.squareDance();
 };
 
 SquareDancer.prototype = Object.create(Dancer.prototype);
@@ -8,4 +9,26 @@ SquareDancer.prototype.constructor = SquareDancer;
 SquareDancer.prototype.step = function(timeBetweenSteps){
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this, timeBetweenSteps);
+};
+
+SquareDancer.prototype.squareDance = function() {
+  var dancer = this;
+
+  setInterval(function() {
+    dancer.$node.animate({
+      left: "+=50"
+    }, 1000, function(){
+      dancer.$node.animate({
+        top: "+=50"
+      }, 1000, function(){
+        dancer.$node.animate({
+          left: "-=50"
+        }, 1000, function(){
+          dancer.$node.animate({
+            top: "-=50"
+          }, 1000);
+        });
+      });
+    });
+  }, 4000);
 };
