07b3c5e69e3c999160f19506229630ebccc6a168
Hack Reactor Students <github@hackreactor.com>
2013-08-13 15:39:17 -0700

    Saved top and left as properties

--- src/dancer.js
+++ src/dancer.js
@@ -1,25 +1,16 @@
-// Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps, defaultNodeType){
-  // use jQuery to create an HTML <span> tag
+  this._top = top;
+  this._left = left;
   this.$node = defaultNodeType || $('<span class="dancer"></span>');
-
   this.step(timeBetweenSteps);
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
   this.setPosition(top, left);
 };
 
 Dancer.prototype.step = function(timeBetweenSteps){
-  // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
   setTimeout(this.step.bind(this, timeBetweenSteps), timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
-  /* Use css top and left properties to position our <span> tag
-   * where it belongs on the page. See http://api.jquery.com/css/
-   */
   var styleSettings = {
     top: top,
     left: left
