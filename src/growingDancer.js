44bc4d4ff0b1c9d2d02f1d8956fec415ca6866a9
Hack Reactor Students <github@hackreactor.com>
2013-08-12 17:40:23 -0700

    added a growingDancer

--- src/growingDancer.js
+++ src/growingDancer.js
@@ -1,5 +1,6 @@
 var GrowingDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.changeSize();
 };
 
 GrowingDancer.prototype = Object.create(Dancer.prototype);
@@ -8,4 +9,16 @@ GrowingDancer.prototype.constructor = GrowingDancer;
 GrowingDancer.prototype.step = function(timeBetweenSteps){
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this, timeBetweenSteps);
+};
+
+GrowingDancer.prototype.changeSize = function() {
+  var dancer = this;
+
+  setInterval(function() {
+    var size = Math.floor(Math.random() * 100) + 10 + 'px';
+    dancer.$node.animate({
+      'border-width': size,
+      'border-radius': size
+    }, 1000);
+  }, 2000);
 };
